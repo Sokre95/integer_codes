36f0695826f77fad8bf6e8d7f41b72001b4ee75d
Krešimir Topolovec <k.topolovec95@gmail.com>
2019-12-05 18:10:51 +0100

    Fix segmentation error. Add simple equals assert

--- c_impl/test.c
+++ c_impl/test.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <assert.h>
+
 #include "leb128.h"
 
 #define DATA_SIZE 8
 
+void ensure_equal(uint32_t *data, uint32_t *decoded, const uint32_t size){
+  int i;
+  for (int i = 0; i < size; ++i){
+    assert (data[i] == decoded[i]);
+  }
+}
+
 int main(int argc, char const *argv[])
 {
   uint32_t data[DATA_SIZE] = { 123, 12, 451, 1, 10000000, 800, 45, 150 };
-  uint8_t encoded[DATA_SIZE];
+  uint8_t encoded[DATA_SIZE * sizeof(uint32_t)];
   uint32_t decoded[DATA_SIZE];
   int i;
 
@@ -21,6 +30,8 @@ int main(int argc, char const *argv[])
   for(i = 0; i < DATA_SIZE; i++){
     printf("%d ", *(decoded + i));
   }
+  // Make sure decoded and input date are equal
+  ensure_equal(data, decoded, DATA_SIZE);
 
   float compression = (float) ( DATA_SIZE * sizeof(uint32_t) ) / encoded_size;
   printf("\nCompression ratio: %.2f\n", compression);
